01c4dc25bfd40be828cecdf85e85e0dfb52c2950
刘玉升 <1250444859@qq.com>
2017-04-12 11:36:31 +0800

    第一次提交ActivityRouter源码

---
+++ app/src/main/java/com/github/mzule/activityrouter/App.java
@@ -0,0 +1,41 @@
+package com.github.mzule.activityrouter;
+
+import com.github.mzule.activityrouter.annotation.Modules;
+import com.github.mzule.activityrouter.router.RouterCallback;
+import com.github.mzule.activityrouter.router.RouterCallbackProvider;
+import com.github.mzule.activityrouter.router.SimpleRouterCallback;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created by CaoDongping on 4/6/16.
+ */
+@Modules({"app", "sdk"})
+public class App extends Application implements RouterCallbackProvider {
+    @Override
+    public RouterCallback provideRouterCallback() {
+        return new SimpleRouterCallback() {
+            @Override
+            public boolean beforeOpen(Context context, Uri uri) {
+                if (uri.toString().startsWith("mzule://")) {
+                    context.startActivity(new Intent(context, LaunchActivity.class));
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void notFound(Context context, Uri uri) {
+                context.startActivity(new Intent(context, NotFoundActivity.class));
+            }
+
+            @Override
+            public void error(Context context, Uri uri, Throwable e) {
+                context.startActivity(ErrorStackActivity.makeIntent(context, uri, e));
+            }
+        };
+    }
+}
---
+++ app/src/main/java/com/github/mzule/activityrouter/MainActivity.java
@@ -0,0 +1,16 @@
+package com.github.mzule.activityrouter;
+
+import com.github.mzule.activityrouter.annotation.Router;
+
+@Router(value = {"http://mzule.com/main", "main", "home"},
+        longParams = {"id", "updateTime"},
+        booleanParams = "web",
+        transfer = "web=>fromWeb")
+public class MainActivity extends DumpExtrasActivity {
+
+    @Override
+    public void finish() {
+        setResult(RESULT_OK);
+        super.finish();
+    }
+}
